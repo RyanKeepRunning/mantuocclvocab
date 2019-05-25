e3caa4364e545ece0cd077a7c3902dd266733e48
Ryan <37783403+RyanKeepRunning@users.noreply.github.com>
2019-05-25 22:27:58 +1000

    add vocab

--- client/src/components/educationVocab.js
+++ client/src/components/educationVocab.js
@@ -4,4 +4,19 @@ export default {name:'education',content:[
     {e:'National Quality Framework',c:'国家质量框架'},
     {e:'Australian Qualification Framework',c:'澳大利亚学历资格框架'},
     {e:'retarded student',c:'智力发育迟缓的学生'},
+    {e:'school counsellor',c:'学校辅导员'},
+    {e:'ex-curriculum',c:'课外活动'},
+    {e:'school leaver',c:'离校者'},
+    {e:'aptitude test',c:'能力测试'},
+    {e:'selective school',c:'精英学校'},
+    {e:'school leaver',c:'离校者'},
+    {e:'corporal punishment',c:'体罚'},
+    {e:'benefit',c:'福利金'},
+    {e:'numeracy',c:'算数能力'},
+    {e:'recess',c:'课件休息'},
+    {e:'syllabus,outline',c:'课程大纲'},
+    {e:'bursary',c:'助学金'},
+    {e:'University Admission Index',c:'大学入学排名'},
+    {e:'skills band',c:'技能级别'}
+    
 ]}
